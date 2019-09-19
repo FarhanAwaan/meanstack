e9f1103b5dcdd1397f38369871493f69f26d5ec8
FarhanAwaan <farhanawaan@gmail.com>
2019-09-19 17:43:53 +0500

    learning node with passport auth
    learning node with passport

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const config = require('./config/database');
+
+// connection to mongoDB
+mongoose.connect(config.database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// checking DB connection
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB Connected');
+});
+// checking DB error
+mongoose.connection.on('error', (err) => {
+    console.log('Database Error: '+err);
+});
+
+// app initialization
+const app = express();
+
+const users = require('./routes/users');
+
+// CORS middleware
+app.use(cors());
+
+// Set Stating Folder
+app.use(express.static(path.join( __dirname + '/client')));
+
+// Body Parser middleware
+app.use(bodyParser.json());
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport")(passport);
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send("Invalid Endpoint");
+});
+
+
+// Port number
+const port = 3000;
+
+// Start Server
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const User = require("../models/User");
+const config = require("../config/database");
+
+// Register
+router.post('/register', (req, res, next) => {
+    let newUser = new User(({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    }));
+
+    User.addUser(newUser, (err, user) => {
+        if(err)
+        {
+            res.json( { success: false, msg: "Failed to register new user" } );
+        }
+        else{
+            res.json( { success: true, msg: "New user registered", data: user } );
+        }
+    });
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByName(username, (err, user) => {
+        if(err)
+            throw err;
+        if(!user)
+        {
+            return res.json({success: false, msg: "user not found"});
+        }
+
+        if(user){
+            User.comparePassword(password, user.password, (err, isMatch) => {
+                if(err) throw err;
+                if(isMatch)
+                {
+                    const token = jwt.sign({data: user}, config.secret, {
+                        expiresIn: '2d'  // 1 week
+                    });
+                    res.json({
+                        success: true,
+                        token: 'JWT'+token,
+                        user: {
+                            id: user._id,
+                            name: user.username,
+                            email: user.email
+                        }
+                    });
+                }
+                else{
+                    return res.json({success: false, msg: "Wrong password entered"});
+                }
+            });
+        }
+    });
+
+});
+
+// User Profile
+router.get('/profile', passport.authenticate('jwt', { session: false } ), (req, res, next) => {
+    console.log("hello");
+    res.json( { user: req.user });
+});
+
+module.exports = router;
